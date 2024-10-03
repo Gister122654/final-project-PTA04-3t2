f217a749629bfb4ae48545035484f30d296773a1
Gister122654 <ltminh7192@gmail.com>
2024-10-03 20:33:16 +0700

    week 4
    sign up ui added

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt6 import uic
 import sys
+class SignUp(QMainWindow):
+    def __init__(self, parent: QWidget | None = ..., flags: Qt.WindowType = ...) -> None:
+        super().__init__()
+        uic.loadUi("ui-GiaoDien/Signup.ui", self)
+        self.btnSignUp.clicked.connect(self.registerAccount)
+    def registerAccount(self):
+        username = self.txtUserName
+        password = self.txtPassWord
+        confirmPassword = self.txtConfirmPassWord
+        #neu chua nhap thi cho ng dung nhap:
+        if username.text() == "":
+            username.setFocus()
+            return
+        if password.text() == "":
+            password.setFocus()
+            return
+        if confirmPassword.text()== "":
+            confirmPassword.setFocus()
+            return
+        if username.text().find("@gmail.com") == -1:
+            username.clear()
+            username.setFocus()
+            return
+        if password.text() != confirmPassword.text():
+            confirmPassword.setFocus()
+            return
 class Login(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ui-GiaoDien/login.ui", self)
         self.btnLogin.clicked.connect(self.checkLogin)
     def checkLogin(self):
-        if self.txtEmail.text() == "SkibidiGooner" and self.txtPassword.text() == "skibidop":
+        with open("account.txt", "r") as file:
+            data = file.readlines()
+            accounts = []
+            for line in data:
+                line = line.replace("\n", "")
+                line = line.strip()
+                accounts.append(line)
+        if (self.txtEmail.text() + ":" + self.txtPassword.text()) in accounts:
             self.close()
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     lg = Login()
-    lg.show()
+    su = SignUp()
+    su.show()
+    # lg.show()
     app.exec()
